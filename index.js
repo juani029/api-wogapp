fdcdd2fed7004df236774db30b84075b9bd0a83e
Juan Ignacio <93682709+juani029@users.noreply.github.com>
2022-11-10 23:29:59 -0300

    add final upd

--- index.js
+++ index.js
@@ -18,11 +18,11 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3002'); // eslint-disable-line no-console
+  server.listen(process.env.PORT || 4000, () => {
+    console.log("%s listening at 3002"); // eslint-disable-line no-console
   });
 });
